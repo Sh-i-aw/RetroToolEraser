5ec18f9c8b1c440a51fee16468ffc5534459344f
vehikl-goldsentintel <goldsentintel@vehikl.com>
2024-07-18 16:43:22 -0400

    wait til window loads

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("Hello world");
+window.onload = () => {
+    function waitUntilReady() {
+        const isReadyToGo = !! document.querySelector('[data-cy="public-section"]')
+
+        if (!isReadyToGo) {
+            setTimeout(waitUntilReady, 500)
+            return;
+        }
+
+        main();
+    }
+
+    waitUntilReady();
+}
+
+
+function main() {
+    alert("Hello world");
+}
